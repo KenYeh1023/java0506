ef7dfaac6615a5c284e470fa98b71287ec95a7da
yaodehaung <sef1548@gmail.com>
2022-05-12 21:12:11 +0800

    feat: sample code

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -5,15 +5,22 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
 @RestController
 public class GreetingController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
-
+	private  List myArrayList = new ArrayList();
 	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	public List read(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return myArrayList;
+	}
+	@GetMapping("/insert")
+	public List insert(@RequestParam(value = "name", defaultValue = "World") int name) {
+		myArrayList.add(name);
+
+		return myArrayList;
 	}
 }
